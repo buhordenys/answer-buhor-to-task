bfc91b979f54479c0ecb5ef91434972f3eac38cc
buhordenys <70490315+buhordenys@users.noreply.github.com>
2020-12-16 10:21:46 +0200

    little spelling changes

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -6,19 +6,19 @@ const Content = (props) => {
     const renderActiveContentTab = () => {
         switch(props.activeTab) {
             case 0: {
-                return <div className="content-tab">...ned add!</div>
+                return <div className="content-tab">...need add!</div>
             }
             case 1: {
-                return <div className="content-tab">...ned add!</div>
+                return <div className="content-tab">...need add!</div>
             }
             case 2: {
                 return <div className="content-tab"><ContentTab/></div>
             }
             case 3: {
-                return <div className="content-tab">...ned add!</div>
+                return <div className="content-tab">...need add!</div>
             }
             case 4: {
-                return <div className="content-tab">...ned add!</div>
+                return <div className="content-tab">...need add!</div>
             }
             default: {
                 return null
